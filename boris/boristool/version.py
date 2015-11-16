f6828dd11193f49468a4f8e0829e86768f88cf0f
Mark Rees <mark.john.rees@gmail.com>
2015-11-16 12:25:34 +1100

    Use setuptools for install

--- boris/boristool/version.py
+++ boris/boristool/version.py
@@ -1,1 +1,5 @@
 version = '0.0.1'
+
+
+def get_version():
+    return version
---
+++ boris/scripts/boris_agent.py
@@ -0,0 +1,6 @@
+"""Command-line entry point for development"""
+
+from boristool.commands import agent
+
+if __name__ == '__main__':
+    agent()
